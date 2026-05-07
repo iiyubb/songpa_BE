3a0c58231f45943596b833f624e8b2674353cdc8
iiyubb <hiyubin.lee@gmail.com>
2026-05-07 15:41:19 +0900

    260507 refactor

--- 05_java/chap09-polymorphism/src/com/ohgiraffers/section02/abstractclass/Application.java
+++ 05_java/chap09-polymorphism/src/com/ohgiraffers/section02/abstractclass/Application.java
@@ -3,7 +3,20 @@ package com.ohgiraffers.section02.abstractclass;
 public class Application {
     public static void main(String[] args) {
 
+        Warrior warrior = new Warrior();
+        Wizard wizard = new Wizard();
 
+        warrior.attack();
+        wizard.attack();
 
+//        Player player = new Player(); // 추상 클래스는 new로 객체 생성 불가
+
+        Player[] players = new Player[2];
+        players[0] = new Warrior();
+        players[1] = new Wizard();
+
+        for (Player player : players) {
+            player.attack();
+        }
     }
 }
